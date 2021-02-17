ae50280affbbb222893647eae513ef76f0c38904
yuka-dir <yksaito02@gmail.com>
2021-02-17 15:57:59 +0900

    wordpress:update http://localhost:5050/wordpress/wp-admin/setup-config.php

--- srcs/wordpress/srcs/setup.sh
+++ srcs/wordpress/srcs/setup.sh
@@ -1,10 +1,16 @@
 #!bin/sh
 
-# Install wordpress
+## Install wordpress
 wget http://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz -C /var/www/localhost/htdocs
 rm -r latest.tar.gz
 
+mkdir /run/nginx/
 mkdir /run/php/
+
+## Start php(make socket)
 usr/sbin/php-fpm7;
+
+## Start nginx
+nginx -g 'daemon off;'
 # killall -KILL php-fpm7;
