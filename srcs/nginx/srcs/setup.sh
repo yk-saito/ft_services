ccf3eb5536b4db1bddde4a6ec783f26fda62ee7f
yuka-dir <yksaito02@gmail.com>
2021-02-18 18:00:26 +0900

    [nginx]:update ssh

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -7,6 +7,8 @@ chmod 755 /var/www/localhost/htdocs/index.html
 #SSL
 openssl req -new -newkey rsa:2048 -nodes -days 365 -x509 -subj "/C=JP/ST=TOkyo/L=Minato-City/O=42Tokyo/OU=./CN=localhost" -keyout /etc/ssl/private/services.key -out /etc/ssl/certs/services.crt
 
+# Start SSH
+/usr/sbin/sshd
 
 # Start nginx
 nginx -g 'daemon off;'
