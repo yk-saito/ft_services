ac97419e656e4dc2cd2041fd05864f7a929fbee9
yuka-dir <yksaito02@gmail.com>
2021-02-23 23:47:51 +0900

    [ftps]:execution conf. docker run -p 21:21 -p 21000-21010:21000-21010 -d --name ftps ftps

---
+++ srcs/ftps/srcs/setup.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+
+#SSL
+openssl req -new -newkey rsa:2048 -nodes -days 365 -x509 -subj "/C=JP/ST=TOkyo/L=Minato-City/O=42Tokyo/OU=./CN=localhost" -keyout /etc/ssl/private/services.key -out /etc/ssl/certs/services.crt
+
+# Start SSH
+/usr/sbin/sshd
+
+# Start ftps
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
