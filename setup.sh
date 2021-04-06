a0ff61be4e27b690b897aed5922a3fb12e818a17
ysaito <shiva.lts239@gmail.com>
2021-04-06 18:18:54 +0900

    make yamlfile for grafana and influxdb

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Environment variable
-METALLB_VER='v0.9.5'
+METALLB_VER='v0.9.6'
 
 # Color
 GREEN=$'\e[0;32m'
@@ -14,41 +14,36 @@ minikube start
 #
 eval $(minikube docker-env)
 
-# 
+#
 echo Docker build images
 docker build -t nginx:ysaito ./srcs/nginx
 docker build -t mysql:ysaito ./srcs/mysql
 docker build -t phpmyadmin:ysaito ./srcs/phpmyadmin
 docker build -t wordpress:ysaito ./srcs/wordpress
 docker build -t ftps:ysaito ./srcs/ftps
-#docker build -t influxdb:ysaito srcs/influxdb
-#docker build -t grafana:ysaito srcs/grafana
+docker build -t influxdb:ysaito srcs/influxdb
+docker build -t grafana:ysaito srcs/grafana
 echo "${GREEN}Successfully created docker images.${COLOR_RESET}"
 
-# Start metalLB
+# Install metalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 #kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/${METALLB_VER}/manifests/namespace.yaml
 #kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/${METALLB_VER}/manifests/metallb.yaml
 #kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-#kubectl apply -f ./srcs/yamls/metallb.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f ./srcs/yamls/metallb-system.yaml
-
 kubectl apply -f ./srcs/yamls/nginx.yaml
 kubectl apply -f ./srcs/yamls/mysql.yaml
 kubectl apply -f ./srcs/yamls/phpmyadmin.yaml
 kubectl apply -f ./srcs/yamls/wordpress.yaml
 kubectl apply -f ./srcs/yamls/ftps.yaml
-#kubectl apply -f ./srcs/influxdb/influxdb.yaml
-#kubectl apply -f ./srcs/grafana/grafana.yaml
+kubectl apply -f ./srcs/influxdb/influxdb.yaml
+kubectl apply -f ./srcs/grafana/grafana.yaml
 
 echo "${GREEN}Successfully execute kubectl.${COLOR_RESET}"
-
-#minikube service nginx-svc
-#minikube service wordpress-svc
-#minikube service phpmyadmin-svc
 
 # Open Kubernetes Dashboard
 minikube dashboard
