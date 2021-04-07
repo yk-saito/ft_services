3a692d8136640acfc113cda54f22b65acb547961
yuka-dir <yksaito02@gmail.com>
2021-04-07 17:35:41 +0900

    change wrdpress/phpmyadmin srcs/default.conf with ssl

--- srcs/phpmyadmin/srcs/setup.sh
+++ srcs/phpmyadmin/srcs/setup.sh
@@ -10,6 +10,9 @@ rm -r phpMyAdmin-5.0.4-all-languages.tar.gz;
 mkdir /run/nginx/
 mkdir /run/php/
 
+## SSL
+openssl req -new -newkey rsa:2048 -nodes -days 365 -x509 -subj "/C=JP/ST=TOkyo/L=Minato-City/O=42Tokyo/OU=./CN=localhost" -keyout /etc/ssl/private/services.key -out /etc/ssl/certs/services.crt
+
 ## Start php(make socket)
 usr/sbin/php-fpm7;
 
--- srcs/wordpress/srcs/setup.sh
+++ srcs/wordpress/srcs/setup.sh
@@ -8,6 +8,9 @@ rm -r latest.tar.gz
 mkdir /run/nginx/
 mkdir /run/php/
 
+#SSL
+openssl req -new -newkey rsa:2048 -nodes -days 365 -x509 -subj "/C=JP/ST=TOkyo/L=Minato-City/O=42Tokyo/OU=./CN=localhost" -keyout /etc/ssl/private/services.key -out /etc/ssl/certs/services.crt
+
 ## Start php(make socket)
 usr/sbin/php-fpm7;
 
