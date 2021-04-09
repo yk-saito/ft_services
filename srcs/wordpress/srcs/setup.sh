1e195e59238d7f1f0b32f9a187cf7ace75a3393e
yuka-dir <yksaito02@gmail.com>
2021-04-09 23:22:00 +0900

    make wordpress add users

--- srcs/wordpress/srcs/setup.sh
+++ srcs/wordpress/srcs/setup.sh
@@ -1,9 +1,22 @@
 #!bin/sh
+WP_PATH=/var/www/localhost/htdocs/wordpress/
 
 ## Install wordpress
 wget http://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz -C /var/www/localhost/htdocs
 rm -r latest.tar.gz
+
+## Install WP-CLI
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+if ! $(wp core is-installed --path=$WP_PATH); 
+then
+    wp core install --path=$WP_PATH --url=https://localhost:5050/wordpress --title=ft_services --admin_user=admin42 --admin_password=admin42 --admin_email=admin@wordpress.com --skip-email
+    wp user create --path=$WP_PATH user1 editor@wordpress.com --role=editor --user_pass=edirot
+    wp user create --path=$WP_PATH user2 author@wordpress.com --role=author --user_pass=author
+fi
 
 mkdir /run/nginx/
 mkdir /run/php/
