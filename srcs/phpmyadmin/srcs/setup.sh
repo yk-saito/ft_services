12a14d8d22970a6abfba89a8d150ed3b346d3f19
yuka-dir <yksaito02@gmail.com>
2021-04-11 21:54:22 +0900

    set phpmyadmin configfile

--- srcs/phpmyadmin/srcs/setup.sh
+++ srcs/phpmyadmin/srcs/setup.sh
@@ -1,12 +1,4 @@
 #!bin/sh
-
-# Install phpmyadmin
-mkdir -p /var/www/localhost/htdocs/phpmyadmin;
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.tar.gz;
-tar -xzvf phpMyAdmin-5.0.4-all-languages.tar.gz\
-	--strip-components=1 -C /var/www/localhost/htdocs/phpmyadmin/;
-rm -r phpMyAdmin-5.0.4-all-languages.tar.gz;
-
 mkdir /run/nginx/
 mkdir /run/php/
 
