e21da7f69e1f7c39e4e3b0efd99d30ec286a70a6
yuka-dir <yksaito02@gmail.com>
2021-04-12 20:36:58 +0900

    fix phpmyadmin error

--- srcs/phpmyadmin/srcs/config.inc.php
+++ srcs/phpmyadmin/srcs/config.inc.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = 'admin42'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'laksdj1fklasjdflkRH6asdfNlk4rfFsadfjafd0aAsdT'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
@@ -29,11 +29,11 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '127.0.0.1';     //mysql-svc
+$cfg['Servers'][$i]['host'] = 'mysql-svc';
 $cfg['Servers'][$i]['port'] = "3306";
 $cfg['Servers'][$i]['user'] = "admin42";
 $cfg['Servers'][$i]['password'] = "admin42";
-#$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
@@ -42,32 +42,32 @@ $cfg['Servers'][$i]['AllowNoPassword'] = true;
  */
 
 /* User used to manipulate with storage */
-#$cfg['Servers'][$i]['controlhost'] = 'localhost';  //mysql-svc
+$cfg['Servers'][$i]['controlhost'] = 'mysql-svc';
 $cfg['Servers'][$i]['controlport'] = '3306';
 $cfg['Servers'][$i]['controluser'] = 'admin42';
 $cfg['Servers'][$i]['controlpass'] = 'admin42';
 
 /* Storage database and tables */
-$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
-$cfg['Servers'][$i]['relation'] = 'pma__relation';
-$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
-$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
-$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
-$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
-$cfg['Servers'][$i]['history'] = 'pma__history';
-$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
-$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
-$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
-$cfg['Servers'][$i]['recent'] = 'pma__recent';
-$cfg['Servers'][$i]['favorite'] = 'pma__favorite';
-$cfg['Servers'][$i]['users'] = 'pma__users';
-$cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
-$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
-$cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
-$cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
-$cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
-$cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
+#$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
+#$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+#$cfg['Servers'][$i]['relation'] = 'pma__relation';
+#$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+#$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+#$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+#$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+#$cfg['Servers'][$i]['history'] = 'pma__history';
+#$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+#$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+#$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+#$cfg['Servers'][$i]['recent'] = 'pma__recent';
+#$cfg['Servers'][$i]['favorite'] = 'pma__favorite';
+#$cfg['Servers'][$i]['users'] = 'pma__users';
+#$cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+#$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+#$cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
+#$cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
+#$cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
+#$cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
 
 /**
  * End of servers configuration
@@ -78,6 +78,8 @@ $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
  */
 $cfg['UploadDir'] = '';
 $cfg['SaveDir'] = '';
+$cfg['TempDir'] = '/tmp';
+$cfg['PmaAbsoluteUri'] = 'https://phpmyadmin-svc:5000';
 
 /**
  * Whether to display icons or text or both icons and text in table row
