023fba68062136727537f78adff10bdd415aef5b
yuka-dir <yksaito02@gmail.com>
2021-04-18 20:27:57 +0900

    test

--- srcs/ftps/srcs/setup.sh
+++ srcs/ftps/srcs/setup.sh
@@ -1,16 +1,13 @@
 #!bin/sh
 
 # SSL
-openssl req -new -newkey rsa:2048 -nodes -days 365 -x509 \
-	-subj "/C=JP/ST=TOkyo/L=Minato-City/O=42Tokyo/OU=./CN=localhost" \
-	-keyout /etc/ssl/private/services.key \
-	-out /etc/ssl/certs/services.crt
+openssl req -new -newkey rsa:2048 -nodes -days 365 -x509 -subj "/C=JP/ST=TOkyo/L=Minato-City/O=42Tokyo/OU=./CN=localhost" -keyout /etc/ssl/private/services.key -out /etc/ssl/certs/services.crt
 
 # Start telegraf
 telegraf --config /etc/telegraf.conf &
 
 # Start SSH
-/usr/sbin/sshd
+#/usr/sbin/sshd
 
 # Start ftps
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
