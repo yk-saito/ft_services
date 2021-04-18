da69b9eb24674d97d28327a35930ca5b9b7fa7a6
yuka-dir <yksaito02@gmail.com>
2021-04-18 15:34:08 +0900

    fix ftps tls error

--- srcs/grafana/srcs/setup.sh
+++ srcs/grafana/srcs/setup.sh
@@ -5,14 +5,11 @@ openssl req -new -newkey rsa:2048 -nodes -days 365 -x509 -subj "/C=JP/ST=TOkyo/L
 sed -i -e "s|protocol = http|protocol = https|g" /var/lib/grafana/conf/defaults.ini
 
 # Setup ssl grafana
-#sed -i -e "s|cert_file =|cert_file = /etc/ssl/certs/services.crt|g" /var/lib/grafana/conf/defaults.ini
-#sed -i -e "s|cert_key =|cert_key = /etc/ssl/private/services.key|g" /var/lib/grafana/conf/defaults.ini
+sed -i -e "s|cert_file =|cert_file = /etc/ssl/certs/services.crt|g" /var/lib/grafana/conf/defaults.ini
+sed -i -e "s|cert_key =|cert_key = /etc/ssl/private/services.key|g" /var/lib/grafana/conf/defaults.ini
 
 # Start telegraf
 telegraf --config /etc/telegraf.conf &
-
-# Start SSH
-#/usr/sbin/sshd
 
 # Start grafana
 /var/lib/grafana/bin/grafana-server --homepath=/var/lib/grafana
