4962c4b86ebe235f7d681460fa76a76b1ca3590d
yuka-dir <yksaito02@gmail.com>
2021-04-19 01:15:46 +0900

    no fix error vsftpd

--- setup.sh
+++ setup.sh
@@ -3,10 +3,6 @@
 # Environment variable
 METALLB_VER='v0.9.6'
 
-# Color
-GREEN=$'\e[0;32m'
-COLOR_RESET=$'\e[0m'
-
 # Check mysql using port 3306
 sudo netstat -nlpt | grep 3306
 if [ $? = 0 ]; then
@@ -21,9 +17,11 @@ minikube stop
 minikube delete
 minikube start --driver=docker
 
+#sudo chmod -R 777 ~/.minikube
+#sudo chmod -R 777 ~/.kube
+
 eval $(minikube docker-env)
 
-echo Docker build images
 docker build -t nginx:ysaito ./srcs/nginx
 docker build -t mysql:ysaito ./srcs/mysql
 docker build -t phpmyadmin:ysaito ./srcs/phpmyadmin
@@ -31,7 +29,6 @@ docker build -t wordpress:ysaito ./srcs/wordpress
 docker build -t ftps:ysaito ./srcs/ftps
 docker build -t influxdb:ysaito ./srcs/influxdb
 docker build -t grafana:ysaito ./srcs/grafana
-echo "${GREEN}Successfully created docker images.${COLOR_RESET}"
 
 # Create secret
 kubectl create secret generic influxdb-creds \
@@ -49,7 +46,6 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/${METALLB_VER
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 kubectl apply -f ./srcs/yamls/metallb-config.yaml
-#kubectl apply -f ./srcs/yamls/pvc.yaml
 kubectl apply -f ./srcs/yamls/nginx.yaml
 kubectl apply -f ./srcs/yamls/mysql.yaml
 kubectl apply -f ./srcs/yamls/phpmyadmin.yaml
@@ -57,8 +53,6 @@ kubectl apply -f ./srcs/yamls/wordpress.yaml
 kubectl apply -f ./srcs/yamls/ftps.yaml
 kubectl apply -f ./srcs/yamls/influxdb.yaml
 kubectl apply -f ./srcs/yamls/grafana.yaml
-
-echo "${GREEN}Successfully execute kubectl apply Manifest.${COLOR_RESET}"
 
 # Open Kubernetes Dashboard
 minikube dashboard
